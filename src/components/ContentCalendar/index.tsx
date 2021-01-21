712aa7256778873cbc70e6157d19af85c1455866
adriano tsurumaki <adriano.tsurumaki@gmail.com>
2021-01-20 23:20:35 -0300

    Estilizando o calendário e tornando o component WeekField dinâmico

--- src/components/ContentCalendar/index.tsx
+++ src/components/ContentCalendar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { 
     Container,
@@ -6,12 +6,68 @@ import {
     Title,
     Content,
     WeekDay,
-    CalendarDay
+    CalendarDay,
+    WeekField,
+    DayField
 } from './styles';
 
 const ContentCalendar: React.FC = () => {
 
-    const [day, setDay] = useState<Date>(new Date());
+    const [daySelected, setDaySelected] = useState<number>(new Date().getDate());
+    const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth());
+    const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
+
+    const fillDay = useMemo(() => {
+        let daysPerWeekUnformatted = [];
+    
+        const date = new Date(yearSelected, monthSelected, 1);
+        const auxFirst = date.getTime();
+        const dayFirst = date.getDay();
+        daysPerWeekUnformatted.push({date: date.getDate(), day: date.getDay()});
+    
+        let n = 1;
+    
+        for(var i = dayFirst; i > 0; i--) {
+            
+            let dateBefore = new Date(auxFirst - (86400000 * n));
+            
+            daysPerWeekUnformatted.unshift({date: dateBefore.getDate(), day: dateBefore.getDay()});
+    
+            n++;
+        }
+        
+        const auxLast = new Date(yearSelected, monthSelected + 1, 1).getTime();
+        const dayLast = new Date(auxLast - 86400000).getDate();
+
+        for(var j = 1; j < dayLast; j++) {
+            let dateAfter = new Date(auxFirst + (86400000 * j));
+            
+            daysPerWeekUnformatted.push({date: dateAfter.getDate(), day: dateAfter.getDay()});
+        }
+
+        const quantityOfWeek = daysPerWeekUnformatted.reduce((total, element) => {
+            if(element.day === 0)
+                return total + 1;
+            else
+                return total;
+        }, 0);
+
+        daysPerWeekUnformatted = daysPerWeekUnformatted.map((item, index) => {
+            return {week: Math.floor(index / 7), ...item}
+        });
+
+        const daysPerWeek = [];
+
+        for(var k = 0; k < quantityOfWeek; k++) {
+            daysPerWeek.push(
+                daysPerWeekUnformatted.filter(item => item.week === k)
+            );
+        }
+
+        console.log({daysPerWeekUnformatted, daysPerWeek, quantityOfWeek});
+
+        return daysPerWeek;
+    }, [monthSelected, yearSelected]);
 
     return (
         <Container>
@@ -22,16 +78,28 @@ const ContentCalendar: React.FC = () => {
             </Header>
             <Content>
                 <WeekDay>
-                    <span>Domingo</span>
-                    <span>Segunda</span>
-                    <span>Terça</span>
-                    <span>Quarta</span>
-                    <span>Quinta</span>
-                    <span>Sexta</span>
-                    <span>Sábado</span>
+                    <span>Dom</span>
+                    <span>Seg</span>
+                    <span>Ter</span>
+                    <span>Qua</span>
+                    <span>Qui</span>
+                    <span>Sex</span>
+                    <span>Sab</span>
                 </WeekDay>
                 <CalendarDay>
-
+                    {fillDay.map(week => {
+                        return (
+                            <WeekField>
+                                {week.map(day => {
+                                    return (
+                                        <DayField>
+                                            <span>{day.date}</span>
+                                        </DayField>
+                                    )
+                                })}
+                            </WeekField>
+                        )
+                    })}
                 </CalendarDay>
             </Content>
         </Container>
--- src/components/ContentCalendar/styles.ts
+++ src/components/ContentCalendar/styles.ts
@@ -8,14 +8,14 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
-    margin: 40px 0 0 40px;
+    margin: 30px 0 0 50px;
 `;
 
 export const Title = styled.div`
     font-size: 18px;
     color: ${props => props.theme.colors.black};
     font-weight: 400;
-    margin-bottom: 25px;
+    margin-bottom: 30px;
 `;
 
 export const Content = styled.div`
@@ -23,11 +23,49 @@ export const Content = styled.div`
 `;
 
 export const WeekDay = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+
+    > span {
+        display: flex;
+        justify-content: center;
+        font-weight: bold;
+        font-size: 18px;
+        cursor: default;
+    }
+
+    margin-bottom: 20px;
 `;
 
 export const CalendarDay = styled.div`
     
+`;
+
+export const WeekField = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+`;
+
+export const DayField = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 10px 0;
+    border-top: 2px solid ${props => props.theme.colors.gray};
+
+    > span {
+        display: flex;
+        justify-content: center;
+        cursor: pointer;
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        border-radius: 50%;
+        transition: all .3s;
+    }
+
+    > span:hover {
+        color: white;
+        background-color: #FF4D6F;
+        box-shadow: 0 0 5px rgba(0,0,0,.5);
+    }
 `;
